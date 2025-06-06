3f06c7d5f25fa4d0f3ad30dd06a1ddb250d48478
ccavalieri <133131478+ccavalieri@users.noreply.github.com>
2025-06-06 15:15:28 -0300

    Create __init__.py

---
+++ SegmentFinder/__init__.py
@@ -0,0 +1,11 @@
+"""
+SegmentFinder - Biblioteca para descoberta de segmentos com uplift positivo em testes A/B
+"""
+
+from .optimizer import BruteForceFinder
+
+__version__ = "0.1.0"
+__author__ = "Seu Nome"
+__email__ = "seu.email@exemplo.com"
+
+__all__ = ["BruteForceFinder"]
